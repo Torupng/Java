96269a8154840339cc7e80f6c4fb8ee0790fcb8d
unknown <Cetecom@SJAPCA03LC1008.duoc.acad>
2024-01-16 21:27:03 -0300

    aknsdabdjd

---
+++ Spotifity/src/main/java/cl/duoc/spotifity/Spotifity.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duoc.spotifity;
+
+import cl.duoc.spotifity.models.Cancion;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class Spotifity {
+
+    public static void main(String[] args) {
+        List<Cancion> musica = new ArrayList();
+        
+        System.out.println("Hello World!");
+    }
+}
---
+++ Spotifity/src/main/java/cl/duoc/spotifity/models/Cancion.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.duoc.spotifity.models;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class Cancion {
+    private String titulo;
+    private String artista;
+    private LocalTime duracion;
+    private boolean favorita;
+    private boolean descarga;
+
+    public Cancion() {
+        this.titulo = "";
+        this.artista = "";
+        this.duracion = LocalTime.now();
+        this.favorita = false;
+        this.descarga = false;
+    }
+    
+    public Cancion(String titulo, String artista, LocalTime duracion, boolean favorita, boolean descarga) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+        this.favorita = favorita;
+        this.descarga = descarga;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public LocalTime getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(LocalTime duracion) {
+        this.duracion = duracion;
+    }
+
+    public boolean isFavorita() {
+        return favorita;
+    }
+
+    public void setFavorita(boolean favorita) {
+        this.favorita = favorita;
+    }
+
+    public boolean isDescarga() {
+        return descarga;
+    }
+
+    public void setDescarga(boolean descarga) {
+        this.descarga = descarga;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "titulo=" + titulo + ", artista=" + artista + ", duracion=" + duracion + ", favorita=" + favorita + ", descarga=" + descarga + '}';
+    }
+    
+    
+}
