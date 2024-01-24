92f8893c5bc0a07369f8c578db24f70436c2d91c
toru <venegasvp08@gmail.com>
2024-01-24 15:28:51 -0300

    xd

---
+++ EjerciciosGraficos/src/main/java/cl/duoc/ejerciciosgraficos/EjerciciosGraficos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.duoc.ejerciciosgraficos;
+
+/**
+ *
+ * @author mangl
+ */
+public class EjerciciosGraficos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
